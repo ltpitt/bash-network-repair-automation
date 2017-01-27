717b7a6bf0862b206e44cc98764881fc67c82077
Davide Nastri <d.nastri@gmail.com>
2017-01-27 13:10:33 +0100

    doc: Simplified, integrated and corrected comments

--- network_check.sh
+++ network_check.sh
@@ -24,15 +24,16 @@ clear
 # Write here the gateway / website you want to check to declare network working or not
 gateway_ip='www.google.com'
 
-# Here we specify the path of temporary file that counts network failures
+# Here we specify the path of a txt file that counts network failures
 network_check_tries_file='/home/pi/scripts/network_check/network_check_tries.txt'
 
-# We put into a variable its content
+# Time to put into a variable its content
 network_check_tries=`cat $network_check_tries_file`
 
-# We check if ping to gateway if working and perform the ok / ko actions
+# Then we check if ping to the specified gateway is working
 host_status=$(fping $gateway_ip)
 
+#  If host is / is not alive we perform the ok / ko actions that simply involve increasing or resetting the failure counter
 if [[ $host_status == *"alive"* ]]
 then
     echo "Network is working correctly" && echo 0 > $network_check_tries_file 
@@ -40,16 +41,16 @@ else
     echo "Network is down..." && echo $(($network_check_tries + 1)) > $network_check_tries_file
 fi
 
-# If network test failed more than 5 times
+# If network test failed more than 5 times (you can change this value to whatever you prefer)
 if [ $network_check_tries -gt 5 ]; then
 echo "Network was not working for the previous $network_check_tries checks."
-# We restart wlan0
+# Time to restart wlan0
     echo "Restarting wlan0"
     /sbin/ifdown 'wlan0'
     sleep 5
     /sbin/ifup --force 'wlan0'
     sleep 30
-# Then we check again if restarting wlan0 fixed the issue, if not we reboot and forcefsck
+# Then we check again if restarting wlan0 fixed the issue, if not we reboot as last resort
     host_status=$(fping $gateway_ip)
     if [[ $host_status == *"alive"* ]]
     then
@@ -57,5 +58,4 @@ echo "Network was not working for the previous $network_check_tries checks."
     else
         echo "Network is down..." && echo 0 > $network_check_tries_file && reboot
     fi
-
 fi
