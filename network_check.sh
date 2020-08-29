bad21571ba10f984f107f48da838b7a4eed2da1c
Davide Nastri <d.nastri@gmail.com>
2020-08-29 20:27:45 +0200

    doc: Corrected punctuation

--- network_check.sh
+++ network_check.sh
@@ -68,7 +68,7 @@ function restart_wlan {
     # In case the previous command, check_gateways, was NOT successful.
     if [[ $? != 0 ]]; then
         if [ "$reboot_server" = true ]; then
-            # If there's no last boot file or it's older than reboot_cycle
+            # If there's no last boot file or it's older than reboot_cycle.
             if [[ ! -f $last_bootfile || $(find $last_bootfile -mtime +$reboot_cycle -print) ]]; then
                 touch $last_bootfile
                 date_log "Network is still not working, rebooting"
